7128204024f4cf394103ecc38edc0e759aca2bb5
kvatoferma <jimmaggot@mail.ru>
2018-09-26 17:10:56 +0300

    Add prime and factorize alg

--- src/index.js
+++ src/index.js
@@ -1,63 +1,76 @@
-module.exports =
+// module.exports =
 function getZerosCount(number, base) {
-//   console.log('number = ', number);
-//   console.log('base = ', base);
 
-  const countDigits = number => {
-    return number.toString().length;
-  }
-
-  const transformTo10 = (number, base) => {
-    const digitQuantity = countDigits(number);
-    const digits = number .toString()
-                        .split('')
-                        .map(digit => parseInt(digit));
-    // console.log('digits: ', digits);
-    // return digits.reduce((sum, digit, i) => {
-    //   console.log('i = ', 0);
-    //   sum += Math.floor(digit * base ** (digitQuantity - i - 1));
-    //   return sum;
-    // }, 0);
-    let sum = 0;
-    for (var i = 0; i < digitQuantity; i++) {
-      // console.log('i = ', i);
-      sum += Math.floor(digits[i] * base ** (digitQuantity - i - 1));
+  const makePrimesTo = num => {
+    const makeNumbersTo = num => {
+      let numbers = [];
+      for (let i = 2; i <= num; i++) {
+        numbers.push(i);
+      }
+      return numbers;
     }
-    return sum;
 
+    const removeMultiple = (numbers, p) => {
+      numbers.forEach((number, i) => {
+        if ((number !== p) && (number % p === 0)) {
+          numbers.splice(i,1);
+        }
+      });
+      console.log('prime numbers: ', numbers);
+      return numbers;
+    }
 
-    //
-    // for (var i = 1; i <= digitQuantity ; i++) {
-    //   result += Math.floor(number * base ** (digitQuantity - i) / 10 ** (digitQuantity - i));
-    // }
-    // return result;
+    let numbers = makeNumbersTo(num);
+    for (let i = 0; i < numbers.length; i++) {
+      while (numbers[i]**2 <= num) {
+        removeMultiple(numbers, numbers[i]);
+        i++;
+      }
+    }
+    return numbers;
   }
 
-
-
-
-// console.log('tranform 2x1010011 into 123: ', transformTo10(11111111, 2));
-// console.log('tranform 0x32 into 50: ', transformTo10(32, 16));
-
-
-//
-  const number10 = transformTo10(number, base);
-  // console.log('number10: ', number10);
-  let zeros = 0;
-  let j = 5;
-  while (number10 / j >= 1) {
-    zeros += Math.floor(number10 / j);
-    j *= 5;
+  const rlog = (num, base) => {
+    if (num == 0) return NaN;
+    if (num == 1) return 0;
+    let power = 0;
+    while (num % base === 0) {
+      power++;
+      num /= base;
+    }
+    return power;
   }
-return zeros;
+
+  console.log('rlog() check');
+  console.log('rlog(16,2) = ', rlog(16,2));
+  console.log('rlog(0,2) = ', rlog(0,2));
+  console.log('rlog(1,2) = ', rlog(1,2));
+
+
+  const factorize = num => {
+    let powers = {};
+    const primes = makePrimesTo(num);
+      primes.forEach(prime => {
+        // if (num % prime === 0) {
+        // let power = Math.floor(Math.log(num) / Math.log(prime));
+        let power = rlog(num, prime);
+        powers[prime] = power;
+        console.log('prime: ', prime, ' power: ', power);
+        num = num / (prime ** power);
+        console.log('new number: ', num);
+      });
+    return powers;
+  }
+
+  let factorizedBase = factorize(base);
+
+  console.log('factorize number: ', factorizedNumber);
+
+
+
+
+
+
 }
 
-//
-// const zerosCount = getZerosCount(46899647, 232);
-// console.log('result actual: ', zerosCount);
-// console.log('result expected: ', '1674985');
-
-
-// it('2', () => {
-  // assert.equal(zerosCount, 1674985);
-// });
+getZerosCount(50, 50);
